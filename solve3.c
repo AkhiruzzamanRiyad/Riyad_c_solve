91c9af49c9e7fb33212edd987cd0b2dbb913c5b3
AkhiruzzamanRiyad <akhiruzzamanriyad960@gmail.com>
2025-08-07 16:55:31 +0600

    guess game update

--- solve3.c
+++ solve3.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(){
     int X,chance=3,gues1,gues2,gues3;
+    int flag=0;
     printf("Player1 choose the Number: ");
     scanf("%d",&X);
     printf("Player-2 gues number :");
@@ -8,29 +9,37 @@ int main(){
 
     if(gues1==X){
         printf("right,player 2 win\n");
+        flag=1;
     }else{
         printf("Wrong answer: chance left %d\n",chance-1);
-
+        flag=0;
         printf("player2 geus again : ");
         scanf("%d",&gues2);
         if(gues2==X){
             printf("Right,player-2 win\n");
+            flag=1;
         }else{
             printf("wrong,chance left %d\n",chance-2);
-
+            flag=0;
             printf("player-2 gues again : ");
             scanf("%d",&gues3);
 
             if (gues3==X)
             {
                 printf("right,player-2 win\n");
+                flag=1;
             }else{
                 printf("wrong,chance left : %d\n",chance-3);
-                printf("Player-1 Win\n");
+                flag=0;
             }
 
         }
     }
+    if (flag==0)
+    {
+        printf("Player-1 Win\n"); 
+    }
+    
     return 0;
 }
 
